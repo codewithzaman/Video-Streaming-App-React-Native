52dcf9550ef182e6b893eefb209246ffae71fce4
Nader Zaman <naderzaman50@gmail.com>
2023-12-23 12:27:45 +0500

    YTPlayer

---
+++ App/Components/YTPlayer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const YTPlayer = () => {
+  return (
+    <View>
+      <Text>YTPlayer</Text>
+    </View>
+  )
+}
+
+export default YTPlayer
+
+const styles = StyleSheet.create({})
